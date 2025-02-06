597784439c341aa7198f47cd07d2e28b8c63ca58
Gilles Gonzalez Oropeza <gilles.gonzalezoropeza@gmail.com>
2025-02-06 21:23:35 +0100

    using a config file for settings

--- scripts/n_bees_value.gd
+++ scripts/n_bees_value.gd
@@ -2,5 +2,17 @@ extends Label
 
 @export var nBeesSlider: Slider
 
+func _ready() -> void:
+	var config = ConfigFile.new()
+	# Load data from a file.
+	var err = config.load("user://config.cfg")
+
+	# If the file didn't load, ignore it.
+	if err != OK:
+		return
+	self.text = str(config.get_value("Settings", "NBees"))
+	
+	nBeesSlider.value = config.get_value("Settings", "NBees")
+
 func _on_n_bees_slider_value_changed(value: float) -> void:
 	self.text = str(value)
