5c1963187bfd7d1ee00076790d3e56604f62ac39
Gilles Gonzalez Oropeza <gilles.gonzalezoropeza@gmail.com>
2025-02-08 18:10:46 +0100

    Fix dragable target on web export

--- scripts/target.gd
+++ scripts/target.gd
@@ -16,24 +16,12 @@ func _process(delta: float) -> void:
 func followMouse():
 	position = get_global_mouse_position() + mouse_offset
 
-#func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
-			 #print("hye")
-				## Debugging the global mouse position
-				#
-#
-				## Get the sprite's global bounding box
 			var sprite_rect_global = Rect2(sprite.global_position, sprite.texture.get_size())
-			print("Global Mouse Position: ", event.position)
-				## Debugging the global sprite bounding box
-			print("Sprite Global Position: ", sprite.global_position)
-			print("Sprite Global Rect: ", sprite_rect_global)
-			var local_mouse = ((event.position / camera.zoom) + camera.offset)
-			print("Local Mouse Position: ", local_mouse)
-				## Check if the global mouse position is inside the sprite's global bounding box
-			if sprite_rect_global.has_point(local_mouse):
+			sprite_rect_global.position = (sprite_rect_global.position - camera.offset) * camera.zoom
+			if sprite_rect_global.has_point(event.position):
 				mouse_offset = position - get_global_mouse_position()
 				selected = true
 		else:
